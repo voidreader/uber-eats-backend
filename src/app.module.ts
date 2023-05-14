f22d497f59257631ee47102c56267ae3c37ee3e1
Radiogaga.jin <radiogaga.jin@gmail.com>
2023-05-14 22:13:42 +0900

    TypeORM 준비

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
@@ -11,6 +12,17 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
     RestaurantsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'gagaHold#3',
+      database: 'nuber-eats',
+      entities: [],
+      synchronize: true,
+      logging: true,
+    }),
   ],
   controllers: [],
   providers: [],
