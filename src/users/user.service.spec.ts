001fef205ed59e554ae7966dd80c9b413656f5f7
gameless <radiogaga.jin@gmail.com>
2023-06-19 17:28:15 +0900

    Jest 연습하기

--- src/users/user.service.spec.ts
+++ src/users/user.service.spec.ts
@@ -8,12 +8,13 @@ import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import { Verification } from './entities/verification.entity';
 import { UserService } from './users.service';
+import { string } from 'joi';
 
-const mockRepository = {
+const mockRepository = () => ({
   findOne: jest.fn(),
   save: jest.fn(),
   create: jest.fn(),
-};
+});
 
 const mockJwtService = {
   sign: jest.fn(),
@@ -27,8 +28,11 @@ const mockMailService = {
 type MockRepository<T> = Partial<Record<keyof Repository<T>, jest.Mock>>;
 
 describe('UserService', () => {
+  // 선언하고, TestingModule에서 값을 할당해야한다.
   let service: UserService;
   let usersRepository: MockRepository<User>;
+  let verificationsRepository: MockRepository<Verification>;
+  let mailService: MailService;
 
   beforeAll(async () => {
     const module = await Test.createTestingModule({
@@ -36,11 +40,11 @@ describe('UserService', () => {
         UserService,
         {
           provide: getRepositoryToken(User),
-          useValue: mockRepository,
+          useValue: mockRepository(),
         },
         {
           provide: getRepositoryToken(Verification),
-          useValue: mockRepository,
+          useValue: mockRepository(),
         },
         {
           provide: JwtService,
@@ -55,6 +59,8 @@ describe('UserService', () => {
 
     service = module.get<UserService>(UserService);
     usersRepository = module.get(getRepositoryToken(User));
+    verificationsRepository = module.get(getRepositoryToken(Verification));
+    mailService = module.get<MailService>(MailService);
   }); // end beforeAll
 
   it(`it should be dfined`, () => {
@@ -62,6 +68,12 @@ describe('UserService', () => {
   });
 
   describe('createAccount', () => {
+    const createAccountArgs = {
+      email: 'xxx@naver.com',
+      password: 'xxxx',
+      role: 0,
+    };
+
     it('should fail if user exists', async () => {
       // DB에 유저가 있으면 fail
       usersRepository.findOne.mockResolvedValue({
@@ -69,18 +81,60 @@ describe('UserService', () => {
         email: 'radiogaga.jin@gmail.com',
       });
 
-      const result = await service.createAccount({
-        email: '',
-        password: '',
-        role: 0,
-      });
+      const result = await service.createAccount(createAccountArgs);
 
       expect(result).toMatchObject({
         ok: false,
         error: 'There is a user with that email already',
       });
     });
-  });
+
+    it(`should create a new user`, async () => {
+      usersRepository.findOne.mockResolvedValue(undefined);
+      usersRepository.create.mockReturnValue(createAccountArgs);
+      usersRepository.save.mockResolvedValue(createAccountArgs);
+      verificationsRepository.create.mockReturnValue({
+        user: createAccountArgs,
+      });
+      verificationsRepository.save.mockResolvedValue({ code: 'code' });
+
+      // 서비스 호출하고.
+      const result = await service.createAccount(createAccountArgs);
+
+      // 여러 다른 함수를 통해서 expect.
+      expect(usersRepository.create).toHaveBeenCalledTimes(1); // 함수가 1번 불릴것이다.
+      // expect(usersRepository.create).toHaveBeenCalledWith('potato'); // 인수가 무엇이었는지?
+      expect(usersRepository.create).toHaveBeenCalledWith(createAccountArgs); // 인수가 무엇이었는지?
+      expect(usersRepository.save).toHaveBeenCalled();
+      expect(usersRepository.save).toHaveBeenCalledWith(createAccountArgs);
+      expect(verificationsRepository.create).toHaveBeenCalled();
+      expect(verificationsRepository.create).toHaveBeenCalledWith({
+        user: createAccountArgs,
+      });
+
+      expect(verificationsRepository.save).toHaveBeenCalled();
+      expect(verificationsRepository.save).toHaveBeenCalledWith({
+        user: createAccountArgs,
+      });
+
+      expect(mailService.sendVerificationEmail).toHaveBeenCalled();
+      expect(mailService.sendVerificationEmail).toHaveBeenCalledWith(
+        expect.any(String),
+        expect.any(String),
+      );
+
+      expect(result).toEqual({ ok: true });
+    }); // ? END it
+
+    it('should fail an exception', async () => {
+      usersRepository.findOne.mockRejectedValue(new Error('whatever!'));
+      try {
+        const result = await service.createAccount(createAccountArgs);
+        console.log(result);
+        expect(result).toEqual({ ok: false, error: "Couldn't create account" });
+      } catch (e) {}
+    }); // END OF it.
+  }); // ? end of describe('createAccount', () => {
   //   it.todo(`createAccount`);
 
   it.todo(`login`);
